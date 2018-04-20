d1747f4254dcedf0d07be477dc512a569264bf95
unexist <none@none>
2018-04-20 13:41:25 +0200

    Added button and mode switch

--- pixellamp/pixellamp.ino
+++ pixellamp/pixellamp.ino
@@ -11,24 +11,16 @@
 
 #include <Adafruit_NeoPixel.h>
 
+/* LED stuff */
+#define PIXEL_COUNT 37
+#define PIXEL_PIN 10
+#define PIXEL_TYPE (NEO_RGB + NEO_KHZ800)
 
-// Parameter 1 = number of pixels in strip
-// Parameter 2 = pin number (most are valid)
-// Parameter 3 = pixel type flags, add together as needed:
-//   NEO_RGB     Pixels are wired for RGB bitstream
-//   NEO_GRB     Pixels are wired for GRB bitstream
-//   NEO_KHZ400  400 KHz bitstream (e.g. FLORA pixels)
-//   NEO_KHZ800  800 KHz bitstream (e.g. High Density LED strip)
+/* Button */
+#define BUTTON_PIN 3
 
-#define PIXEL_COUNT 27
-#define PIXEL_PIN 6
-#define PIXEL_TYPE NEO_RGB + NEO_KHZ800
-
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);
-
-////
-// User Variables
-////
+/* Debug */
+//#define DEBUG 1
 
 uint32_t decayTime = 2835;                  // Start dimming light after elapsed seconds
 uint32_t decayDelay = 3;                    // Seconds between decay fade-out steps
@@ -37,10 +29,6 @@ uint16_t maxDayBrightness = 180;            // 0 - 255, lamp will not exceed thi
 uint16_t maxDuskBrightness = 70;            // 0 - 255, lamp will not exceed this during dusk
 uint16_t maxNightBrightness = 20;           // 0 - 255, lamp will not exceed this during the night
 float fadeRate = 0.95;                      // Fireworks Variable: 0.01-0.99, controls decay speed
-
-////
-// End User Variables
-////
 
 double lastColorUpdate = 0;                 // Epoch of last color update (local or remote)
 String colorFromID;                         // String, Tracks who sent the color (for debug)
@@ -62,47 +50,71 @@ float greenStates[PIXEL_COUNT];             // Fireworks Variable
 uint8_t heartbeatDirector = 0;              // Heartbeat Tracking
 uint8_t heartbeatColor = 0;                 // Heartbeat Tracking
 
+/* State */
+volatile uint8_t buttonState = 0;
+volatile uint8_t lampMode = 0;
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);
+
 void setup() {
   strip.begin();
   strip.show();
+
+  /* Button */
+  pinMode(BUTTON_PIN, INPUT);
+  attachInterrupt(0, pin_ISR, CHANGE);
+
+#ifdef DEBUG
+  Serial.begin(9600);
+#endif /* DEBUG */      
+}
+
+void pin_ISR() {
+  buttonState = digitalRead(BUTTON_PIN);
+#ifdef DEBUG
+      Serial.print(buttonState);
+      Serial.println();
+#endif /* DEBUG */
+
+  if(HIGH == buttonState) {
+    if(6 > lampMode) lampMode++;
+    else lampMode = 0;
+  }
 }
 
 void loop() {
-  /*unsigned long secs = millis() / 1000;
-
-    maxBrightness = maxDayBrightness;
-    if (lampBrightness > maxBrightness) lampBrightness = maxBrightness;
-    setColor(activeColor);*/
-
-  //rainbowFull(30, 2);
-  ///idleFireworks(0);
-  //idleColorFlicker(21);
-  //idleDisco();
-  //idleColorFader(0,85);
-
-
-  rainbowFull(30, 0, 1);
-
-  //whileTouching();
-
-  /*if (secs - lastColorUpdate > decayTime) {
-    if (secs - lastDecayDelay >= decayDelay) {
-      extinguish();
-      lastDecayDelay = secs;
-    }
-    }*/
+#ifdef DEBUG
+      Serial.print(lampMode);
+      Serial.println();
+#endif /* DEBUG */
+  
+  switch(lampMode) {
+    case 0: idleFireworks(0); break;    
+    case 1: rainbowFull(30, 1, 0); break;    
+    case 2: idleColorFlicker(21); break;
+    case 3: idleDisco(); break;
+    case 4: idleColorFader(0, 85); break;
+    case 5: idleHeartbeat(); break;
+    case 6: extinguish(); break;
+  }
 }
 
+/**
+ * Set color
+ **/
+ 
 void setColor(byte c) { // c is color.  This function does a "random dither" to set the new color
   // Determine highest bit needed to represent pixel index
   uint32_t color = wheelColor(c, lampBrightness);
   int hiBit = 0;
   int n = strip.numPixels() - 1;
-  for (int bit = 1; bit < 0x8000; bit <<= 1) {
+  
+  for (unsigned int bit = 1; bit < 0x8000; bit <<= 1) {
     if (n & bit) hiBit = bit;
   }
 
   int bit, reverse;
+  
   for (int i = 0; i < (hiBit << 1); i++) {
     // Reverse the bits in i to create ordered dither:
     reverse = 0;
@@ -114,21 +126,33 @@ void setColor(byte c) { // c is color.  This function does a "random dither" to 
     strip.show();
     delay(20);
   }
+  
   activeColor = c;
 }
+
+/**
+ * Extinguish lamp
+ **/
 
 void extinguish() { //Dims the lamp by one unit until lampBrightness is 0 and lampOn is 0
   lampBrightness--;
   uint32_t color = wheelColor(activeColor, lampBrightness);
+  
   for (byte j = 0; j <= strip.numPixels(); j++) {
     strip.setPixelColor(j, color);
   }
+  
   strip.show();
+  
   if (lampBrightness <= 0) {
     lampOn = 0; //If the lamp is completely off, set lampOn to 0
     lampBrightness = 0; // Make sure this number isn't negative somehow
   }
 }
+
+/**
+ * Select color from color wheel
+ **/
 
 uint32_t wheelColor(uint16_t WheelPos, uint16_t iBrightness) {
   float R, G, B;
@@ -152,80 +176,64 @@ uint32_t wheelColor(uint16_t WheelPos, uint16_t iBrightness) {
   activeR = R * brightness;// + .5;
   activeG = G * brightness;// + .5;
   activeB = B * brightness;// + .5;
+  
   return strip.Color((byte) activeR, (byte) activeG, (byte) activeB);
 }
 
-void whileTouching() {
-  byte previousBrightness = lampBrightness; // Store the previous brightness in case we need it later
-  uint16_t pixelBrightness = lampBrightness; // Tracks the given pixel's brightness.  Needs to track > 255, so uint16_t
-  uint8_t testColor = activeColor; // Start with the current color
-  activePixels = 0;
-
-  for (byte k = 0; k < 30; k++) {
-    for (byte i = 0; i <= activePixels; i++) {
-      pixelBrightness = lampBrightness + i; //Fade to full brightness
-      if (pixelBrightness > maxBrightness) pixelBrightness = maxBrightness; //catch overflow
-      // "activePixels - i" reverses the direction
-      strip.setPixelColor(activePixels - i, wheelColor(((i * 60 / strip.numPixels()) + testColor) & 255, pixelBrightness)); // "& 255" AKA bitwise and prevents overflow
-    }
-    strip.show();
-    testColor++; //because testColor is uint8_t, automatically loops at 256
-    if (activePixels < (strip.numPixels() - 1)) activePixels++; //Add 1 for next iteration, but prevent looping around
-    if (lampBrightness < maxBrightness) lampBrightness++;
-    delay(3);
-  }
-
-  if (activePixels >= (strip.numPixels() - 10)) {
-    lampOn = 1;
-    activeColor = testColor;
-  } else {
-    lampBrightness = previousBrightness;
-    setColor(activeColor);
-  }
-}
+/**
+ * Ranbow colors
+ **/
 
 void rainbowFull(byte wait, byte fade, byte pendulum) {
+  uint8_t lastMode = lampMode;
   uint16_t i, j, k;
   byte dir = 1;
   if (fade == 0) k = 0;
   else k = maxBrightness;
 
-  while (true) {
+  for (j = 0; j <= 255 && lastMode == lampMode; j++) {
+    for (i = 0; i < strip.numPixels() && lastMode == lampMode; i++) {
+      strip.setPixelColor((strip.numPixels() - 1) - i, 
+        wheelColor(((i * 60 / strip.numPixels()) + j) & 255, k));
+    }
 
-    for (j = 0; j <= 255; j++) {
-      for (i = 0; i < strip.numPixels(); i++) {
-        strip.setPixelColor((strip.numPixels() - 1) - i, wheelColor(((i * 60 / strip.numPixels()) + j) & 255, k));
-      }
+    strip.show();
+    delay(wait);
 
-      strip.show();
-      delay(wait);
-
-      if ((1 == pendulum && 1 == dir) || (fade == 0 && k < maxBrightness)) {
-        k++;
-        if (k == maxBrightness) dir = 2;
-      } else if ((1 == pendulum && 2 == dir) || (fade == 2 && k > 0)) {
-        if (1 == k) dir = 1;
-        k--;
-      }
+    if ((1 == pendulum && 1 == dir) || (fade == 0 && k < maxBrightness)) {
+      k++;
+      if (k == maxBrightness) dir = 2;
+    } else if ((1 == pendulum && 2 == dir) || (fade == 2 && k > 0)) {
+      if (1 == k) dir = 1;
+      k--;
     }
   }
 }
 
+/**
+ * Simple color fader
+ **/
+
 void idleColorFader(uint8_t c1, uint8_t c2) {
+  uint8_t lastMode = lampMode;
   lampBrightness = 40;
+  
   if ( maxBrightness < lampBrightness ) {
     lampBrightness = maxBrightness;
   }
+  
   uint16_t currR, currG, currB, endR, endG, endB;
   uint32_t color = wheelColor(fadeColor, lampBrightness);
   endR = (uint16_t)((color >> 16) & 0xff); // Splits out new color into separate R, G, B
   endG = (uint16_t)((color >> 8) & 0xff);
   endB = (uint16_t)(color & 0xff);
-  for (uint16_t j = 0; j < activePixels; j++) {
+  
+  for (uint16_t j = 0; j < activePixels && lastMode == lampMode; j++) {
     long startRGB = strip.getPixelColor(j); // Get pixel's current color
     currR = (uint16_t)((startRGB >> 16) & 0xff); // Splits out current color into separate R, G, B
     currG = (uint16_t)((startRGB >> 8) & 0xff);
     currB = (uint16_t)(startRGB & 0xff);
+    
     if ( currR > endR ) {
       currR = currR - 1;
     } else if ( currR < endR ) {
@@ -268,12 +276,20 @@ void idleColorFader(uint8_t c1, uint8_t c2) {
   delay(20);
 }
 
+/**
+ * Simple fireworks
+ **/
+
 void idleFireworks(uint8_t w) {
+  uint8_t lastMode = lampMode;
+
   // w = 0 for mulitcolor, w = 1 for all white flashes
   lampBrightness = 40;
+  
   if ( maxBrightness < lampBrightness ) {
     lampBrightness = maxBrightness;
   }
+  
   if (random(20) == 1) {
     uint16_t i = random(strip.numPixels());
     if (redStates[i] < 1 && greenStates[i] < 1 && blueStates[i] < 1) {
@@ -288,7 +304,8 @@ void idleFireworks(uint8_t w) {
       }
     }
   }
-  for (uint16_t l = 0; l < strip.numPixels(); l++) {
+  
+  for (uint16_t l = 0; l < strip.numPixels() && lastMode == lampMode; l++) {
     if (redStates[l] > 1 || greenStates[l] > 1 || blueStates[l] > 1) {
       strip.setPixelColor(l, redStates[l], greenStates[l], blueStates[l]);
       if (redStates[l] > 1) {
@@ -315,41 +332,68 @@ void idleFireworks(uint8_t w) {
   }
   strip.show();
 }
+
+/**
+ * Simple disco lights
+ **/
+
 void idleDisco() {
-  lampBrightness = 20;
+  uint8_t lastMode = lampMode;
+  lampBrightness = 40;
+  
   if ( maxBrightness < lampBrightness ) {
     lampBrightness = maxBrightness;
   }
-  for (int i = 0; i < strip.numPixels(); i++) {
+  
+  for (unsigned int i = 0; i < strip.numPixels() && lastMode == lampMode; i++) {
     int randr = random(0, lampBrightness);
     int randg = random(0, lampBrightness);
     int randb = random(0, lampBrightness);
     int randi = random(0, (strip.numPixels() - 1));
+    
     strip.setPixelColor(randi, randr, randg, randb);
     strip.show();
+    
     delay(5);
   }
 }
+
+/**
+ * Simple color flicker
+ **/
+
 void idleColorFlicker(uint8_t c) {
+  uint8_t lastMode = lampMode;
   lampBrightness = 20;
+  
   if ( maxBrightness < lampBrightness ) {
     lampBrightness = maxBrightness;
   }
+  
   uint32_t color = wheelColor(c, lampBrightness);
 
-  for (uint8_t i = 0; i < strip.numPixels(); i++) {
+  for (uint8_t i = 0; i < strip.numPixels() && lastMode == lampMode; i++) {
     uint8_t flicker = random(0, 10);
+    
     int flickerR = (uint16_t)((color >> 16) & 0xff) - flicker; // Splits out new color into separate R, G, B
     int flickerG = (uint16_t)((color >> 8) & 0xff) - flicker;
     int flickerB = (uint16_t)(color & 0xff) - flicker;
+    
     if (flickerR < 0) flickerR = 0;
     if (flickerG < 0) flickerG = 0;
     if (flickerB < 0) flickerB = 0;
+    
     strip.setPixelColor(i, flickerR, flickerG, flickerB);
   }
+  
   strip.show();
   delay(30);
 }
+
+/**
+ * Simple heartbeat
+ **/
+
 void idleHeartbeat() {
   lampBrightness = 20;
   if ( maxBrightness < lampBrightness ) {
