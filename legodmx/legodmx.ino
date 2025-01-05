b83a86b8871c286dfcf66b36955667b3d200838d
Christoph Kappel <christoph@unexist.dev>
2025-01-05 16:34:39 +0100

    Updated sketch

--- legodmx/legodmx.ino
+++ legodmx/legodmx.ino
@@ -9,213 +9,64 @@
  * See the file LICENSE.
  **/
 
-#include <SPI.h>
 #include <Conceptinetics.h>
 
-/* LED stuff */
-#define LED_DDR          DDRB
-#define LED_PORT         PORTB
-#define LED_PIN          (1 << PORTB5)
+//#define DEBUG 1
 
-#define GROUPS           6                                          ///< Total LED groups
-#define LED_PER_GROUP    6                                          ///< LEDs per group
-#define CHANNEL_PER_LED  3                                          ///< Channel per LED (typically RGB=3)
-#define CONTROL_CHAN     4                                          ///< Number of special channel
-#define CHANNELS         (GROUPS * CHANNEL_PER_LED + CONTROL_CHAN)  ///< Total channel for DMX
-#define COLORS           (GROUPS * CHANNEL_PER_LED * LED_PER_GROUP) ///< Total colors
-
-char colors[COLORS] = { 0 };
+/* Mosfet stuff */
+#define MOS_PIN       6
 
 /* DMX stuff */
-#define STARTADDR  217
-#define RX_PIN     3
-#define WX_PIN     4
+#define DMX_ADDRESS   1
+#define DMX_CHANNELS  1
+#define DMX_RX_PIN    3
+#define DMX_WX_PIN    4
 
-DMX_Slave slave(CHANNELS, RX_PIN);
+DMX_Slave slave(DMX_CHANNELS, DMX_RX_PIN);
 
-/* Presets */
-#define RED   255,   0,  0
-#define GREEN   0, 255, 255
-#define BLUE    0,   0, 255
+#ifdef DEBUG
+char buf[255] = { 0 };
+#endif /* DEBUG */
 
-/* transfer
- * Transfer data via SPI and wait for finish
- * @param  data  Data to write
- **/
-
-void
-transfer(volatile char data)
-{
-  /* Copy data */
-  SPDR = data;
-
-  /* Poll bit and wait for end of the transmission */
-  while(!(SPSR & (1 << SPIF)));
-}
-
-/** updateCols
- *  Write color values to SPI
- **/
-
-void
-updateCols()
-{
-  for(char i = 0; i < COLORS; i++)
-    {
-      transfer(colors[i]);
-    }
-}
-
-/** setUnlessZero
- * Set value when not zero
- * @param  color  Color var
- * @param  value  New value
- **/
-
-void
-setUnlessZero(char *color, 
-  char value)
-{
-  if(0 != value) *color = value;
-}
-
-/** setForGroups
- * Set RGB color for given group
- * @param  gid  ID of group to set colors
- * @param  r    Red value
- * @param  g    Green value
- * @param  b    Blue value
- **/
-
-void
-setForGroup(int gid,
-  char r,
-  char g,
-  char b)
-{
-  for(char curgid = 0, pos = (gid * LED_PER_GROUP * CHANNEL_PER_LED);
-      curgid < LED_PER_GROUP; curgid++, pos += CHANNEL_PER_LED)
-    {
-      colors[pos]     = r;
-      colors[pos + 1] = g;
-      colors[pos + 2] = b;
-    }
-}
-
-/** onFrameReceived
- *  Handle frame receival
+/**
+ *  Handle frame recieval
  *  @param  channels  Channels that was set
  **/
 
-void
-onFrameReceived(short unsigned int channel)
-{
-  /* 1) Set colors for color groups */
-  for(char led = 0, pos = 0, chan = (CONTROL_CHAN + 1); led < GROUPS; led++) ///< Channel count starts at 1!
-    {     
-      for(char posoff = 0, chanoff = 0; posoff < (LED_PER_GROUP * CHANNEL_PER_LED); posoff++)
-        {
-          colors[pos + posoff] = slave.getChannelValue(chan + chanoff++);
+void onFrameReceived(short unsigned int channel) {
+    int dmxval = slave.getChannelValue(1);
+  
+    analogWrite(MOS_PIN, dmxval);
 
-          if(CHANNEL_PER_LED == chanoff) chanoff = 0;
-        }
+#ifdef DEBUG
+    snprintf(buf, sizeof(buf), "chan=%d, val=%d\n", channel, dmxval);
 
-      chan += CHANNEL_PER_LED;
-      pos  += (LED_PER_GROUP * CHANNEL_PER_LED);
-    }
-
-  /* 2) Control: Set color for all LEDs */
-  for(char pos = 0; pos < COLORS; pos += CHANNEL_PER_LED)
-    {
-      setUnlessZero(&colors[pos],     slave.getChannelValue(2));
-      setUnlessZero(&colors[pos + 1], slave.getChannelValue(3));
-      setUnlessZero(&colors[pos + 2], slave.getChannelValue(4));
-    }
-
-  /* 3) Control: Special mode */
-  char  mode   = slave.getChannelValue(1);
-  short cols[] = { 0, 0, 255, 0, 0, 255, 0 };
-  char  sel    = 0;
-
-  for(char gid = 0; 10 <= mode && gid < GROUPS; gid++)
-    {
-      char mod = (gid % CHANNEL_PER_LED);
-
-      /* Pick selector function */
-      if(10 <= mode && 20 > mode)
-        {
-          sel = 4 - mod; ///< G-B-R
-        }
-      else if(20 <= mode && 30 > mode)
-        {
-          sel = 3 - mod; ///< B-R-G
-        }
-      else if(30 <= mode && 40 > mode)
-        {
-          sel = 2 - mod; ///< R-G-B
-        }
-      else if(40 <= mode && 50 > mode)
-        { 
-          sel = mod; ///< B-G-R         
-        }
-      else if(50 <= mode && 60 > mode)
-        {          
-          sel = 1 + mod; ///< G-R-B
-        }
-      else if(60 <= mode && 70 > mode)
-        {          
-          sel = 2 + mod; ///< R-B-G
-        }
-      /* Solid colors */
-      else if(210 <= mode && 220 > mode)
-        {
-          sel = 2; ///< R
-        }
-      else if(220 <= mode && 230 > mode)
-        {
-          sel = 1; ///< G
-        }
-      else if(230 <= mode && 255 > mode)
-        {
-          sel = 0; ///< B
-        }
-
-      setForGroup(gid, cols[sel], cols[sel + 1], cols[sel + 2]);
-    }
-
-  updateCols(); 
+    Serial.print(buf);
+#endif /* DEBUG */
 }
 
-/** setup
+/**
  * Set up arduino
  **/
  
-void 
-setup()
-{
-  /* Init LED and SPI */
-  LED_DDR  |=  LED_PIN; ///< Enable output for LED
-  LED_PORT &= ~LED_PIN; ///< LED off
+void setup() {
+    /* Init pins */
+    pinMode(MOS_PIN, OUTPUT);
 
-  SPI.begin();
-  SPI.setBitOrder(MSBFIRST);
-  SPI.setDataMode(SPI_MODE0);
-  SPI.setClockDivider(SPI_CLOCK_DIV16); ///< 1 MHz max, else flicker
+    /* Init and start DMX slave */
+    slave.enable();  
+    slave.setStartAddress(DMX_ADDRESS);
+    slave.onReceiveComplete(onFrameReceived);
 
-  updateCols();
-
-  /* Init and start slave */
-  slave.enable ();  
-  slave.setStartAddress(STARTADDR);
-  slave.onReceiveComplete(onFrameReceived);
+#ifdef DEBUG
+    Serial.begin(9600);
+#endif /* DEBUG */
 }
 
-/** loop
+/**
  * Run in loop
  **/
 
-void
-loop()
-{
-  /* We do nothing here */
+void loop() {
+    /* We do nothing here */
 }
